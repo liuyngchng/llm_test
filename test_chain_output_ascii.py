ae565c3108974e160f4fbf58b498cf4ed9b55636
richard <whoami@hotmail.com>
2025-03-05 09:50:54 +0800

    debug

--- test_chain_output_ascii.py
+++ test_chain_output_ascii.py
@@ -9,22 +9,24 @@ class Joke(BaseModel):
     setup: str = Field(description="question to set up a joke")
     punchline: str = Field(description="answer to resolve the joke")
 
+def get_chain():
+    chat_model = ChatOllama(model="llama3.2:3B", temperature=0)
 
-chat_model = ChatOllama(model="llama3.2:3B", temperature=0)
+    joke_query = "Tell me a joke."
 
-joke_query = "Tell me a joke."
+    prompt_template = ChatPromptTemplate.from_messages(
+        [
+            ("system", "Answer the user query.\n{format_instructions}"),
+            ("user", "{query}")
+        ])
 
-prompt_template = ChatPromptTemplate.from_messages(
-    [
-        ("system", "Answer the user query.\n{format_instructions}"),
-        ("user", "{query}")
-    ])
+    chain = prompt_template | chat_model
+    chain.invoke(
+        {"query": joke_query,
+         "format_instructions": chat_model.get_output_schema()
+         })
+    print("print ascii of the chain")
+    chain.get_graph().print_ascii()
 
-chain = prompt_template | chat_model
-
-chain.invoke(
-    {"query": joke_query,
-     "format_instructions": chat_model.get_graph()
-     })
-print("print ascii of the chain")
-chain.get_graph().print_ascii()
+if __name__ == "__main__" :
+    get_chain()
