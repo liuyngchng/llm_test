bd45730d451c9b57c3130361e4c3b7af1dd5fc44
richard <whoami@hotmail.com>
2025-03-06 16:51:47 +0800

    debug

--- requst_llm.py
+++ requst_llm.py
@@ -11,22 +11,16 @@ def _set_env(key: str):
         os.environ[key] = getpass.getpass(f"{key}:")
 
 
-_set_env("OPENAI_API_KEY")
-
-
 def req(question):
     """
     请求大模型 API，获取返回的信息
     :param question:
     :return:
     """
-    # question = "hi"
-    # model_name = "deepseekR17B"
-    # llm_url = "http://127.0.0.1:11434"
+    question = "hi"
+    model_name = "deepseekR17B"
+    llm_url = "http://127.0.0.1:11434"
 
-    model_name = "deepseek-r1"
-    llm_url = "https://aiproxy.petrotech.cnpc/v1"
-    api_key = "sk-myuqX43dtcWnjoXD8a54F80f6a0143E583Fb07E15b188c91"
     # 加载配置
     logging.config.fileConfig('logging.conf')
 
@@ -38,4 +32,5 @@ def req(question):
     logger.info("answer is {}".format(answer))
 
 if __name__ == "__main__":
+    _set_env("OPENAI_API_KEY")
     req("hi")
